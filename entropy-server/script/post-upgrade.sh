59530ca478ad3028c34809d2425d0bee27273d2d
mudler <mudler@sabayon.org>
2016-02-12 10:43:39 +0100

    creating entropy-server image, inside builder for convenience

---
+++ entropy-server/script/post-upgrade.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+PACKAGES_TO_REMOVE=(
+)
+
+FILES_TO_REMOVE=(
+   "/.viminfo"
+   "/.history"
+   "/.zcompdump"
+   "/var/log/emerge.log"
+   "/var/log/emerge-fetch.log"
+)
+
+PACKAGES_TO_ADD=(
+    "sys-apps/entropy-server"
+)
+
+rsync -av -H -A -X --delete-during "rsync://rsync.at.gentoo.org/gentoo-portage/licenses/" "/usr/portage/licenses/"
+ls /usr/portage/licenses -1 | xargs -0 > /etc/entropy/packages/license.accept
+
+# upgrading machine
+equo up && equo u
+
+# Handling install/removal of packages specified in env
+equo i "${PACKAGES_TO_ADD[@]}"
+
+# Merging defaults configurations
+echo -5 | equo conf update
+
+# Writing package list file
+equo q list installed -qv > /etc/sabayon-pkglist
+
+# Cleaning equo package cache
+equo cleanup
+
+# Remove scripts
+rm -rf /post-upgrade.sh
+
+# Cleanup
+rm -rf "${FILES_TO_REMOVE[@]}"
